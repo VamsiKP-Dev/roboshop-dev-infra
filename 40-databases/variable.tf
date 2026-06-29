27e45ffac03c36c78ee0dfed11517a3401f4347b
Vamsi Krishna P <vamsikrishnap@hotmail.com>
2026-06-29 19:35:08 +0530

    changes added

---
+++ 40-databases/variable.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+  default = "Z03822472J12MWULO7O9D"
+}
+
+variable "domain_name" {
+  default = "daws90s.sbs"
+}
